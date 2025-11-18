f383138c6d5e1698fe6135133ecf4ce0361b7e35
watari <watari@techwebunraveled.xyz>
2025-11-18 13:01:11 +0300

    feat: add NetBox integration with .env support
    Integrate NetBox API to fetch device info for automation scripts.
    Use a .env file to store API URL and token securely

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python3
 
+from dotenv import load_dotenv
+import os
+import pynetbox
+import json
+
+load_dotenv()
+
+NETBOX_URL = os.getenv("NETBOX_URL")
+NETBOX_TOKEN = os.getenv("NETBOX_TOKEN")
+
 def main():
     pass
 
 if __name__ == "__main__":
+    nb = pynetbox.api(
+        NETBOX_URL,
+        token=NETBOX_TOKEN,
+        threading=True,
+        #strict_filters=True,
+    )
+    
+    devices = nb.dcim.devices.filter(device_role="cisco-switch")
+    
+    for d in devices:
+        device_dict = dict(d)
+        print(json.dumps(device_dict, indent=2))
     pass
