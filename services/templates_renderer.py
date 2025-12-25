3bf4153f6248cad3de668343466084f6ef51e2f6
watari <watari@techwebunraveled.xyz>
2025-12-25 16:15:21 +0300

    tacacs+ rendering works

--- services/templates_renderer.py
+++ services/templates_renderer.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 
-env = Environment(loader=FileSystemLoader("templates"))
+env = Environment(loader=FileSystemLoader("services/templates/"))
 
-template = env.from_string("Hello, {{ name }}!")
-print(template.render(name="World"))
+template = env.get_template("tacacs.j2")
+
+print(template.render(TACACS_IP="10.0.0.1", SECRET="cisco"))
