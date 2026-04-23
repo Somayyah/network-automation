494c6ca7f335edad04ad6304b4b2789e8923eeee
watari <watari@techwebunraveled.xyz>
2026-04-23 14:38:22 +0300

    setting sudo roles for new user

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,10 +3,28 @@
 HOST=$1
 USER=$2
 
+echo "Connecting to $HOST as $USER..."
+
+echo "Creating user..."
 ssh "$USER@$HOST" <<EOF
 useradd -m -s /bin/bash ansible
 usermod -aG wheel ansible
-mkdir -p /home/ansible/.ssh
 EOF
 
-# ssh-copy-id "ansible@$HOST"
+echo "Configuring SSH..."
+ssh "$USER@$HOST" <<EOF
+mkdir -p /home/ansible/.ssh
+chown -R ansible:ansible /home/ansible/.ssh
+chmod 700 /home/ansible/.ssh
+
+# TBD...
+
+EOF
+
+echo "Setting sudo rules..."
+ssh "$USER@$HOST" <<EOF
+echo "ansible ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/ansible
+chmod 440 /etc/sudoers.d/ansible
+EOF
+
+echo "Done."
